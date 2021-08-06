d6fdf17da1979b3328be45ef9372a27124d85508
michelalejo <michelmolina65@gmail.com>
2021-08-06 07:50:29 -0500

    main.ts

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -1,52 +1,44 @@
 interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
 };
 
 const student1: Student = {
-    firstName: 'Holberton',
-    lastName: 'School',
-    age: 0,
-    location: 'Colombia',
+  firstName: 'Holberton',
+  lastName: 'School',
+  age: 0,
+  location: 'Colombia',
 };
 
 const student2: Student = {
-    firstName: 'School',
-    lastName: 'Holberton',
-    age: 1,
-    location: 'Usa',
+  firstName: 'School',
+  lastName: 'Holberton',
+  age: 1,
+  location: 'Usa',
 };
 
 const studentsList: Student[] = [student1, student2];
 
-const body: HTMLBodyElement = document.getElementsByTagName('body')[0];
-const table: HTMLTableElement = document.createElement('table');
-const thead: HTMLTableSectionElement = document.createElement('thead');
-const tbody: HTMLTableSectionElement = document.createElement('tbody');
-const tr: HTMLTableRowElement = document.createElement('tr');
-const th1: HTMLTableCellElement = document.createElement('td');
-th1.innerHTML = 'firstName';
-const th2: HTMLTableCellElement = document.createElement('td');
-th2.innerHTML = 'location';
+var body = document.getElementsByTagName("body")[0];
+var tabla = document.createElement("table");
+var tblBody = document.createElement("tbody");
 
-body.append(table);
-table.append(thead);
-table.append(tbody);
-thead.append(tr);
-tr.append(th1);
-tr.append(th2);
-
-studentsList.forEach(({ firstName, location }: Student) => {
-  const trTb: HTMLTableRowElement = document.createElement('tr');
-  tbody.append(trTb);
-
-  let td: HTMLTableCellElement = document.createElement('td');
-  td.innerHTML = firstName;
-  trTb.append(td);
-
-  td = document.createElement('td');
-  td.innerHTML = location;
-  trTb.append(td);
-});
+for (var k = 0; k < 2; k++) {
+  for (var i = 0; i < 4; i++) {
+    var hilera = document.createElement("tr");
+    var celda = document.createElement("td");
+    var textoCelda = document.createTextNode(Object.getOwnPropertyNames(studentsList[k])[i]);
+    celda.appendChild(textoCelda);
+    hilera.appendChild(celda);
+    var celda = document.createElement("td");
+    var textoCelda = document.createTextNode(Object.values(studentsList[k])[i]);
+    celda.appendChild(textoCelda);
+    hilera.appendChild(celda);
+    tblBody.appendChild(hilera);
+  }
+}
+tabla.appendChild(tblBody);
+body.appendChild(tabla);
+tabla.setAttribute("border", "2");
