e8b9018bd3dba2666008c1bdc27c641e5c716ae5
michelalejo <michelmolina65@gmail.com>
2021-08-14 17:31:09 -0500

    change files

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -1,49 +1,52 @@
-interface Student {
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = function (
+  firstName: string,
+  lastName: string
+): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements StudentClassInterface {
   firstName: string;
   lastName: string;
-  age: number;
-  location: string;
-};
 
-const student1: Student = {
-  firstName: 'Holberton',
-  lastName: 'School',
-  age: 0,
-  location: 'Colombia',
-};
-
-const student2: Student = {
-  firstName: 'School',
-  lastName: 'Holberton',
-  age: 1,
-  location: 'Usa',
-};
-
-const studentsList: Student[] = [student1, student2];
-
-var body = document.getElementsByTagName("body")[0];
-var tabla = document.createElement("table");
-var tblBody = document.createElement("tbody");
-
-for (var k = 0; k < 2; k++) {
-  for (var i = 0; i < 4; i++) {
-    var hilera = document.createElement("tr");
-    var celda = document.createElement("td");
-    var textoCelda = document.createTextNode(Object.getOwnPropertyNames(studentsList[k])[i]);
-    celda.appendChild(textoCelda);
-    hilera.appendChild(celda);
-    var celda = document.createElement("td");
-    var textoCelda = document.createTextNode(Object.values(studentsList[k])[i]);
-    celda.appendChild(textoCelda);
-    hilera.appendChild(celda);
-    tblBody.appendChild(hilera);
-  }
-  if (k < 1) {
-    var salto = document.createElement("br");
-    tblBody.appendChild(salto);
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
 
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }
-tabla.appendChild(tblBody);
-body.appendChild(tabla);
-tabla.setAttribute("border", "2");
+
+console.log(printTeacher("John", "Doe"));
