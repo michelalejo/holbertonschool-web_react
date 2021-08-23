7a7d265fcee42e62eaed959e8344297a778959ad
michelalejo <michelmolina65@gmail.com>
2021-08-22 22:32:24 -0500

    7-equality.js

--- 0x07-React_Immutable/7-equality.js
+++ 0x07-React_Immutable/7-equality.js
@@ -1,8 +1,5 @@
-import { Map } from 'immutable';
+import { is } from 'immutable';
 
-export default function mergeDeeplyElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-
-  return map1.mergeDeep(map2);
+export default function areMapsEqual(map1, map2) {
+  return is(map1, map2);
 }
